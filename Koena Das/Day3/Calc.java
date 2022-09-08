c1c52ee802a4f07661f0dfc65da6e272dbe9754f
koko564 <70006867+koko564@users.noreply.github.com>
2022-09-08 14:30:03 +0530

    Add files via upload

---
+++ Koena Das/Day3/Calc.java
@@ -0,0 +1,13 @@
+
+public class Calc {
+
+	public static int addIntegers(int a,int b) {
+		return a+b;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(addIntegers(12,13));
+
+	}
+
+}
